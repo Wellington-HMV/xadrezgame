fa51a74b3fbfb53c12788683ca2898547244445e
Wellington <well.raiser@gmail.com>
2019-06-18 03:14:24 -0300

    Projeto criado

---
+++ Xadrex-game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xadrex_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
