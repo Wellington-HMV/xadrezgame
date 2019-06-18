c0d1f7db08d26bdffcbadd57f553b6113de2e8d0
Wellington <well.raiser@gmail.com>
2019-06-18 03:24:38 -0300

    Position class create

---
+++ Xadrex-game/tabuleiro/Position.cs
@@ -0,0 +1,19 @@
+﻿
+namespace tabuleiro
+{
+    class Position
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Position(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+        public override string ToString()
+        {
+            return Linha + ", " + Coluna;
+        }
+    }
+}
